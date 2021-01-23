23acacb989ea590abbfb135019b83257f0aecdf8
derMacon <silash169@gmail.com>
2021-01-24 00:58:13 +0100

    updated project structure

--- simple-build.sh
+++ simple-build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # set necessary environment varibles
-cd docker/
+cd workshop-organizer/
 source ./export-env.sh
 
 # build spring application
-cd ../workshop-organizer/
+cd spring-project/
 mvn clean package
 
 # build docker image and start container via
 # docker-compose (-d for detached mode)
-cd ../docker/
+cd ../
 docker-compose down
 docker-compose up --build -d 
 
