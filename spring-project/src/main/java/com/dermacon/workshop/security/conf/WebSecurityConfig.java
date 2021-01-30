18fdc734f804d06bff32ab2cbef476cc1620bf6b
derMacon <silash169@gmail.com>
2021-01-30 15:41:57 +0100

    updated course overview

--- spring-project/src/main/java/com/dermacon/workshop/security/conf/WebSecurityConfig.java
+++ spring-project/src/main/java/com/dermacon/workshop/security/conf/WebSecurityConfig.java
@@ -3,10 +3,13 @@ package com.dermacon.workshop.security.conf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,83 +22,112 @@ import java.util.concurrent.TimeUnit;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    /**
+     * Https endpoint for api requests.
+     * Uses basic authentication.
+     */
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            // todo permit /api/admin/ only for admin users
+            http
+                    .csrf().disable()
+                    .antMatcher("/api/**")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .hasAnyRole("USER", "ADMIN")
+                    .and()
+                    .httpBasic();
+        }
+    }
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    @Qualifier("dataSource")
-    private DataSource dataSource;
 
     /**
-     * source: http://cristianruizblog.com/spring-security-persistent-token-2/
-     *
-     * @return
+     * Http endpoint for standard get requests by the browser to display the website.
+     * Uses form login for authentication
      */
-    @Bean
-    public PersistentTokenRepository persistentTokenRepository() {
-        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
-        tokenRepository.setDataSource(dataSource);
-        return tokenRepository;
-    }
+    @Configuration
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/help", "/registration/**", "/css/**").permitAll()
-                .antMatchers("/courses/all").hasAnyRole("USER", "MANAGER", "ADMIN")
-                .antMatchers("/courses/enrolled").hasAnyRole("USER", "MANAGER")
-                .antMatchers("/courses/created").hasAnyRole("MANAGER", "ADMIN")
-                .antMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/css/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/accessDenied")
-                .and()
-                .rememberMe()
-                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(100))
+        @Autowired
+        private UserDetailsService userDetailsService;
+
+        @Autowired
+        @Qualifier("dataSource")
+        private DataSource dataSource;
+
+        /**
+         * source: http://cristianruizblog.com/spring-security-persistent-token-2/
+         *
+         * @return
+         */
+        @Bean
+        public PersistentTokenRepository persistentTokenRepository() {
+            JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+            tokenRepository.setDataSource(dataSource);
+            return tokenRepository;
+        }
+
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/help", "/registration/**", "/css/**").permitAll()
+                    .antMatchers("/courses/all").hasAnyRole("USER", "MANAGER", "ADMIN")
+                    .antMatchers("/courses/enrolled").hasAnyRole("USER", "MANAGER", "ADMIN")
+                    .antMatchers("/courses/created").hasAnyRole("MANAGER", "ADMIN")
+                    .antMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/css/**").permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .formLogin()
+                    .permitAll()
+                    .and()
+                    .exceptionHandling().accessDeniedPage("/accessDenied")
+                    .and()
+                    .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(100))
 //                    .tokenValiditySeconds((int)60)
-                .tokenRepository(persistentTokenRepository())
-                .userDetailsService(userDetailsService)
-                // random key, todo put into application.properties
-                .key("45lk432j5;lj435;l432j");
-    }
+                    .tokenRepository(persistentTokenRepository())
+                    .userDetailsService(userDetailsService)
+                    // random key, todo put into application.properties
+                    .key("45lk432j5;lj435;l432j");
+        }
 
-    @Autowired
-    public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+        @Autowired
+        public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService);
+        }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return super.userDetailsService();
-    }
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return super.userDetailsService();
+        }
 
 
-    // ---------- hashing passwords -----------
+        // ---------- hashing passwords -----------
 
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public static PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
+        @Bean
+        public DaoAuthenticationProvider authProvider() {
+            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(userDetailsService);
+            authProvider.setPasswordEncoder(passwordEncoder());
+            return authProvider;
+        }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(authProvider());
+        }
     }
 }
